8a4a1ff7445957305822d5419d6a731758dfc69f
João Ricardo <ricardo.forttunato@gmail.com>
2024-02-23 12:22:03 -0300

    Improve input visualization in initial asks

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%',  '&', '*', '-', '_','?', '/']
 
 print('Welcome to the PyPassword Generator!')
-nr_letter = int(input('How many letters would you like in your password?'))
-nr_number = int(input('How many numbers would you like in your password?'))
-nr_symbols = int(input('How many symbols would you like in your password?'))
+nr_letter = int(input('How many letters would you like in your password? \n'))
+nr_number = int(input('How many numbers would you like in your password? \n'))
+nr_symbols = int(input('How many symbols would you like in your password? \n'))
 
 nr_password = []
 
